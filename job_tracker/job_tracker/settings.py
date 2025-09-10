536902beaeb4a4f1474607d9e8cb50c8f7768b6e
deepanshu01-dev <deepanshukumar81825@gmail.com>
2025-09-10 10:11:54 +0530

    using postgresql for job application tracker

--- job_tracker/job_tracker/settings.py
+++ job_tracker/job_tracker/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'job_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'user',
+        'PASSWORD': 'pasword',
+        'HOST': 'localhost',
+        'PORT': 'port',
     }
 }
 
